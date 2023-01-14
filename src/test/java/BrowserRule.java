18685fe4f008e3c6445a1c59fb1b2f053fe25050
Alexey Yudin <a-yu2008@yandex.ru>
2023-01-14 15:58:15 +0300

    add some update 1

--- src/test/java/BrowserRule.java
+++ src/test/java/BrowserRule.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserRule extends ExternalResource {
@@ -15,6 +16,11 @@ public class BrowserRule extends ExternalResource {
     protected void before() throws  Throwable {
 
         //Настройка браузера
+
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.setBinary("C:/Users/bucha/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+//        WebDriver driver = new ChromeDriver(options);
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
